51c871582260c7e3702e3de48917fbdb6c725e6f
Larysa1387 <kaliuzhna.off@gmail.com>
2021-09-20 01:39:31 +0300

    add module commander

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # goit-nodejs-homework-01
+
+# Получаем и выводим весь список контактов в виде таблицы (console.table)
+
+node app.js --action list
+https://monosnap.com/file/tTrW0kHAH9QfmFT9LQtevtz0L3wGIW
+
+# Получаем контакт по id
+
+node app.js --action get --id 5
+https://monosnap.com/file/Ys9LobgYATNhWAoW9fjAhGQLoun93S
+
+# Добавялем контакт
+
+node app.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/9bIwS78xjuHd80zmZVzwG1sMKJpyrL
+
+# Удаляем контакт
+
+node app.js --action remove --id=3
+https://monosnap.com/file/ujhjcHw9v5RJCCVwi5P1zS4b5O5eBf
--- app.js
+++ app.js
@@ -1,2 +1,74 @@
 const colors = require("colors");
+const { program } = require("commander");
+const asyncHandler = require("./utils/asyncHandler");
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./contacts");
+
 console.log("It's OK".bold.yellow);
+
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      const contacts = await asyncHandler(listContacts());
+      console.table(contacts);
+      break;
+
+    case "get":
+      // const contact = await asyncHandler(getContactById(id));
+      // console.log(contact);
+      try {
+        const contact = await getContactById(id);
+        if (!contact) {
+          throw new Error(`Such contact with id=${id} doesn't exist`);
+        }
+        console.log(contact);
+      } catch (error) {
+        console.log(error.message.orange);
+      }
+      break;
+
+    case "add":
+      // const newContact = await addContact({ name, email, phone });
+      // await asyncHandler(addContact(newContact));
+      try {
+        const newContact = await addContact({ name, email, phone });
+        console.log(newContact);
+      } catch (error) {
+        console.log(error.message.orange);
+      }
+      break;
+
+    case "remove":
+      // await asyncHandler(removeContact(id));
+      try {
+        const contact = await removeContact(id);
+        if (!contact) {
+          throw new Error(`Such contact ${contact} doesn't exist`);
+        }
+        console.log(`Contact with id=${id} was removed`);
+      } catch (error) {
+        console.log(error.message.orange);
+      }
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
